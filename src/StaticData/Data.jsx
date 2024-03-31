7782ba235ee3e47a82fd4d840cc17d6f304f58a0
jinshhaaad <jinshadpk88@gmail .com>
2024-03-31 11:37:49 +0530

    Initial commit

---
+++ src/StaticData/Data.jsx
@@ -0,0 +1,33 @@
+import Food1 from '../assets/Food1.jpg';
+import Food2 from '../assets/Food2.jpg';
+import Food3 from '../assets/Food3.jpeg';
+
+export const Data = [
+    {
+        id :'1',
+        img:Food1,
+        price:'3000',
+        item:'Combo Dishes',
+        likes:2,
+        heart:2,
+        share:2000,
+    },
+    {
+        id :'2',
+        img:Food2,
+        price:'3500',
+        item:'London Dishes',
+        likes:3,
+        heart:2,
+        share:2400 , 
+    },
+    {
+        id :'3',
+        img:Food3,
+        price:'5000',
+        item:'Shawarma Pro',
+        likes:5,
+        heart:4,
+        share:3000,
+    }
+];
